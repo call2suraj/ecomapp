e1115666e9243e8cd25d033434179a1b44a0d86e
Suraj Behera <call2suraj@gmail.com>
2023-09-10 13:56:22 +0530

    added search

--- src/components/Items.js
+++ src/components/Items.js
@@ -9,25 +9,24 @@ import Grid from '@mui/material/Grid';
 
 export default function Items(props) {
 
-  return (
-    <Grid>
-    <Paper elevation={3} style={{margin: 1}} >
-    <Card style={{height: 410, width: 300}}>
-      <img src={props.img} alt="tv" height={208} width={240} style={{margin: 5}}/>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {props?.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-         {props.desc}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Add To Cart</Button>
-      </CardActions>
-    </Card></Paper>
-</Grid>
-  
-  );
+    return (
+        <Grid>
+            <Paper elevation={3} style={{ margin: 1 }} >
+                <Card style={{ height: 410, width: 300 }}>
+                    <img src={props.img} alt="tv" height={208} width={240} style={{ margin: 5 }} />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {props?.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {props.desc}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Share</Button>
+                        <Button size="small">Add To Cart</Button>
+                    </CardActions>
+                </Card></Paper>
+        </Grid>
+    );
 }
